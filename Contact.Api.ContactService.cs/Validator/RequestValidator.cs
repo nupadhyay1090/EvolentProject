76335f4c7719ea067ec048b984332de702141780
tavisca-nupadhyay <nupadhyay@tavisca.com>
2019-12-10 17:47:35 +0530

    Removing the Service test proj and adding email validator

--- Contact.Api.ContactService.cs/Validator/RequestValidator.cs
+++ Contact.Api.ContactService.cs/Validator/RequestValidator.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using Api.Contracts.Models;
 using Api.Contracts.Error;
+using System.Text.RegularExpressions;
+
 namespace Api.ContactService.Validator
 {
     public static class RequestValidator
     {
         public static void ValidateRequest(Contact contact)
         {
-            if (contact.EmailID.Length > 3)
+            Regex emailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = emailRegex.Match(contact.EmailID);
+
+            if (!match.Success)
                 throw ClientErrors.InvalidEmailId();
 
-            if( contact.FirstName.Length > 49)
-                throw ClientErrors.
+            if (contact.FirstName.Length > 50)
+                throw ClientErrors.InvalidFirstName();
+
+            if (contact.LastName.Length > 50)
+                throw ClientErrors.InvalidLastName();
         }
     }
 }
